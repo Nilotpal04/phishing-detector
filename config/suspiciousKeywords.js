92aa9fb3351aebd8a43fa0974d52d909f177523e
H Jivan Singha <hjivansingha123@gmail.com>
2026-03-17 02:39:46 +0530

    added config : badTLDs , brandDomains , suspiciousKeywords ,trustedDomains

---
+++ config/suspiciousKeywords.js
@@ -0,0 +1,33 @@
+//
+
+const suspiciousKeywords=[
+     // authentication
+    "login",
+    "signin",
+    "verify",
+    "verification",
+
+    // urgency
+    "urgent",
+    "immediate",
+    "alert",
+
+    // banking
+    "bank",
+    "secure",
+    "account",
+    "update",
+    "confirm",
+
+    // common phishing patterns
+    "security",
+    "reset",
+    "password",
+    "wallet",
+
+    // crypto scams
+    "crypto",
+    "airdrop",
+    "bonus",
+    "reward"
+];
